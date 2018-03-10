3eadfe635f93f7cf1960d18e9e0c08c10f2cb96c
mtytel <matthewtytel@gmail.com>
2018-03-10 09:29:25 -0800

    Can drag around target in MoveObjectOnHit

--- Assets/NuvuAssets/Scripts/MoveObjectOnHit.cs
+++ Assets/NuvuAssets/Scripts/MoveObjectOnHit.cs
@@ -6,11 +6,23 @@ public class MoveObjectOnHit : HitTarget
 
     override public void Hit(TouchHitInfo hitInfo)
     {
-        if (objectToMove)
-            objectToMove.position = hitInfo.hitPoint;
+        MoveTo(hitInfo.hitPoint);
+    }
+
+    override public void Move(TouchHitInfo hitInfo)
+    {
+        Ray ray = new Ray(hitInfo.touchSource.position, hitInfo.curDirection);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+            MoveTo(hit.point);
     }
 
     override public void Release(TouchHitInfo hitInfo)
     {
+    }
+
+    void MoveTo(Vector3 position) {
+        if (objectToMove)
+            objectToMove.position = position;
     }
 }
